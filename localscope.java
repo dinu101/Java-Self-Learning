c028f69a4237cf591fb41621f9e433f23f0271a7
dinu101 <dr975208@gmail.com>
2025-08-01 03:32:51 -0700

    initial commit

---
+++ localscope.java
@@ -0,0 +1,11 @@
+class localscope{
+    public static void main(String[] args){
+      int Scope=80;//local varible
+      System.out.println(Scope);
+    }
+  //  System.out.println(Scope);//local variable not accesible here.
+}
+      //Question//
+//Q.What Is Local Scope?
+//Ans:- * A Variable Declared Inside A Method,Constructor Or Block.
+//      * Only Accesible With IN The Block.
