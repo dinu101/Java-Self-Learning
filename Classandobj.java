20ec6f9bffaab306cfe943e2582d101c1376c998
dinu101 <dr975208@gmail.com>
2025-08-02 05:25:32 -0700

    initial Commit

---
+++ Classandobj.java
@@ -0,0 +1,16 @@
+// Define a public class named Classandobj
+public class Classandobj {
+    
+    // Declare an instance variable 'x' and initialize it with value 5
+    int x = 5;
+
+    // Main method: Java program execution starts from here
+    public static void main(String[] args) {
+
+        // Create an object 'myObj' of the Classandobj class
+        Classandobj myObj = new Classandobj();
+
+        // Access and print the value of instance variable 'x' using the object
+        System.out.println(myObj.x);  // Output will be: 5
+    }
+}
